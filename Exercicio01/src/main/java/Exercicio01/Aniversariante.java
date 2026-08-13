595192218e2caaf55df29e4e00b76e25880bb3f1
Ghabriel Camilo <ghabrielcamilo98@gmail.com>
2026-08-13 11:42:17 -0300

    Atividade parcialmente feita

---
+++ Exercicio01/src/main/java/Exercicio01/Aniversariante.java
@@ -0,0 +1,15 @@
+package Exercicio01;
+
+public class Aniversariante {
+	private String nome;
+	private DataAniversario dataAniversario;
+	
+	public Aniversariante (String nome, int dia, String mês) {
+		this.nome = "nome";
+		this.dia = dia;
+		this.mês = mês;		
+	}
+	
+	public Aniversariante (String nome, )
+
+}
